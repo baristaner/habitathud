8e453ed8a49d876099f570e722200b554b326386
Barış Taner <legendbaris2003@gmail.com>
2019-06-20 12:15:29 +0300

    Delete init.lua

--- autorun/server/init.lua
+++
@@ -1,7 +0,0 @@
-local function PrintNoArguments()
-	print( "[HABITATHUD]!" )
-end
-
-timer.Create( "UniqueName3", 60, 0, PrintNoArguments )
-
-hook.Add( "Initialize", "Timer Example", CreateSomeTimers )
